145323749952ddd4dbf1cc0d46ed0018d666b00f
Geren160 <Marcoag160@gmail.com>
2025-02-16 17:00:06 -0700

    Version final Steam
    Se agregaron las funcionalidades faltantes (verificar compra y eliminar del carrito)

--- test/pageobjects/steam.page.js
+++ test/pageobjects/steam.page.js
@@ -4,7 +4,7 @@ const Page = require('./page');
 class steamPage extends Page {
 
     async buscarJuego() {
-        const inputBusqueda = await $('//*[@id="store_nav_search_term"]');
+        const inputBusqueda = await $('//*[@id="store_nav_search_term"]'); //input de busqueda
         await inputBusqueda.waitForDisplayed();
         await inputBusqueda.setValue('Undertale');
         await browser.keys('Enter');
@@ -12,7 +12,7 @@ class steamPage extends Page {
     }
 
     async seleccionarJuego() {
-        const seleccionarJuego = await $('//*[@id="search_resultsRows"]/a[1]/div[2]/div[1]/span');
+        const seleccionarJuego = await $('//*[@id="search_resultsRows"]/a[1]/div[2]/div[1]/span'); //seleccionar juego en los resultados
         await seleccionarJuego.waitForDisplayed();
         await seleccionarJuego.click();
         await browser.pause(2000);
@@ -23,31 +23,42 @@ class steamPage extends Page {
     }
 
     async agregarJuegoAlCarrito() {
-        const agregarAlCarrito = await $('//*[@id="btn_add_to_cart_74780"]/span');
+        const agregarAlCarrito = await $('//*[@id="btn_add_to_cart_74780"]/span'); //Boton agregar al carrito
         await agregarAlCarrito.waitForDisplayed();
         await agregarAlCarrito.click();
         await browser.pause(2000);
     }
 
     get verificarCompra() {
-        return $('/html/body/div[3]/dialog/div/div[2]/div/div[3]/div/div[1]');
+        return $('/html/body/div[3]/dialog/div/div[2]/div/div[3]/div/div[1]'); //Debe decir Added to your cart!
     }
     
     async revisarCarrito() {
-        const revisarCarrito = await $('/html/body/div[3]/dialog/div/div[2]/div/div[3]/div/div[3]/button[1]');
+        const seguirComprando = await $('/html/body/div[3]/dialog/div/div[2]/div/div[3]/div/div[3]/button[1]'); //Le da clic en seguir comprando
+        await seguirComprando.waitForDisplayed();
+        await seguirComprando.click();
+        const revisarCarrito = await $('//*[@id="cart_status_data"]/div/div/a'); //Le da clic en el carro
         await revisarCarrito.waitForDisplayed();
         await revisarCarrito.click();
         await browser.pause(2000);
     }
 
-    async verificarNotificacionCarrito() {
-        const notificacionCarrito = await $('/html/body/div[1]/div[7]/div[6]/div[3]/div[1]/div/div/div[1]/div/div/div/a');
-        await notificacionCarrito.waitForDisplayed();
-        await notificacionCarrito.click();
+    get verificarIconoCarrito() {
+        return $('//*[@id="cart_status_data"]/div/div/a'); //Debe aparecer el icono del carrito
+    }
+
+    async eliminarDelCarrito() {
+        const eliminarDelCarrito = await $('//*[@id="page_root"]/div[2]/div/div[2]/div[3]/div[1]/div[3]/div[2]'); //Hace clic en eliminar todos los articulos
+        await eliminarDelCarrito.waitForDisplayed();
+        await eliminarDelCarrito.click();
         await browser.pause(2000);
     }
 
-    async esperar(milisegundos) {
+    get verificarEliminacion() {
+        return $('//*[@id="page_root"]/div[2]/div/div[2]/div[3]/div[1]/div[1]/div/div'); //Debe aparecer el mensaje de que no hay articulos en el carrito
+    }
+   
+        async esperar(milisegundos) {
         await browser.pause(milisegundos);
     }
 
--- test/specs/test.e2e.js
+++ test/specs/test.e2e.js
@@ -2,42 +2,43 @@ const { expect } = require('@wdio/globals')
 const mercadoPage = require('../pageobjects/mercado.page')
 const steamPage = require('../pageobjects/steam.page')
 
-describe('Navegando en mercado libre', () => {
-    it('Verificar el menu de categorias', async () => {
-        await mercadoPage.open()
-        await mercadoPage.desplegarMenuDeCategorias()
-        await expect(await mercadoPage.menuDeVehiculosInternoEnCategorias).toHaveText('Vehículos')
-        await expect(await mercadoPage.menuDeSupermercadoInternoEnCategorias).toHaveText('Supermercado')
-    })
-
-    it('Verificar el submenu de Tecnología dentro del menu categorias', async () => {
-        await mercadoPage.desplegarMenuDeTecnología()
-        await expect(await mercadoPage.menuDeTecnologíaInternoEnCategorias).toHaveText('Tecnología')
-    })
-
-    it ('abrir el elemento Videojuegos' , async () => {
-        await expect(await mercadoPage.videoJuegosDeSubmenuTecnologia).toHaveText('Videojuegos')
-        await expect(await mercadoPage.videoJuegosDeSubmenuTecnologia).toBeClickable()
-        await mercadoPage.hacerClickEnVideoJuegos()
-    })
-
-})
-
-// describe('Comprando en Steam', () => {
-//     it('Buscar juego', async () => {
-//         await steamPage.open()
-//         await steamPage.buscarJuego()
-//         await steamPage.seleccionarJuego()
-//         await expect(await steamPage.verificarJuego).toHaveText('Undertale')
+// describe('Navegando en mercado libre', () => {
+//     it('Verificar el menu de categorias', async () => {
+//         await mercadoPage.open()
+//         await mercadoPage.desplegarMenuDeCategorias()
+//         await expect(await mercadoPage.menuDeVehiculosInternoEnCategorias).toHaveText('Vehículos')
+//         await expect(await mercadoPage.menuDeSupermercadoInternoEnCategorias).toHaveText('Supermercado')
 //     })
 
-//     it('Agregar juego al carrito', async () => {
-//         await steamPage.agregarJuegoAlCarrito()
-//         await expect(await steamPage.verificarCompra).toHaveText('Added to your cart!')
+//     it('Verificar el submenu de Tecnología dentro del menu categorias', async () => {
+//         await mercadoPage.desplegarMenuDeTecnología()
+//         await expect(await mercadoPage.menuDeTecnologíaInternoEnCategorias).toHaveText('Tecnología')
 //     })
 
-//     it('Eliminar juego del carrito', async () => {
-//         await steamPage.revisarCarrito()
-//         await expect(await steamPage.verificarNotificacionCarrito).toBeDisplayed()
+//     it ('abrir el elemento Videojuegos' , async () => {
+//         await expect(await mercadoPage.videoJuegosDeSubmenuTecnologia).toHaveText('Videojuegos')
+//         await expect(await mercadoPage.videoJuegosDeSubmenuTecnologia).toBeClickable()
+//         await mercadoPage.hacerClickEnVideoJuegos()
 //     })
 // })
+
+describe('Comprando en Steam', () => {
+    it('Buscar juego', async () => {
+        await steamPage.open()
+        await steamPage.buscarJuego()
+        await steamPage.seleccionarJuego()
+        await expect(await steamPage.verificarJuego).toHaveText('Undertale')
+    })
+
+    it('Agregar juego al carrito', async () => {
+        await steamPage.agregarJuegoAlCarrito()
+        await expect(await steamPage.verificarCompra).toHaveText('Added to your cart!')
+        await steamPage.revisarCarrito()
+        await expect(await steamPage.verificarIconoCarrito).toHaveText('Cart (1)')
+    })
+
+    it('Eliminar juego del carrito', async () => {
+        await steamPage.eliminarDelCarrito()
+        await expect(await steamPage.verificarEliminacion).toHaveText('Your cart is empty.')
+    })
+})
